265323129cdec4ba7dec13c044292e673e4e58ca
Alexander <mistodrake@gmail.com>
2022-09-01 16:40:11 +0600

    Finding mouse coordinates

---
+++ Coord.py
@@ -0,0 +1,8 @@
+import pyautogui
+import time
+
+time.sleep(2)
+print(pyautogui.position())
+time.sleep(2)
+print(pyautogui.position())
+
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # text-detector
+requirement:
+1. Opencv
+2. PyAutoGUI
+
